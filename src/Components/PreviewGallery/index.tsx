59cb65cb95f5b4bb90d44edc602f609ed0fab82c
Vladimir Orlov <86786994+Satancrew@users.noreply.github.com>
2023-09-07 17:18:58 +0300

    feat: add styles (#8)

--- src/Components/PreviewGallery/index.tsx
+++ src/Components/PreviewGallery/index.tsx
@@ -2,6 +2,7 @@ import { Photo, CommonClassProps } from '../types';
 import cl from 'classnames';
 
 import style from './index.module.scss';
+import { useEffect, useMemo, useRef } from 'react';
 
 interface PreviewGalleryProps extends CommonClassProps {
   activePhotoIndex: number;
@@ -12,21 +13,43 @@ export const PreviewGallery: React.FC<PreviewGalleryProps> = ({
   activePhotoIndex,
   photos,
   className,
-}) => (
-  <div className={cl(style.previewGallery, className)}>
-    <ul className={style.previewGalleryTrack}>
-      {photos.map(photo => (
-        <li key={photo.id} className={style.previewGalleryPreview}>
-          <img
-            src={photo.preview}
-            alt={photo.alt}
-            className={style.previewGalleryImage}
-          />
-        </li>
-      ))}
-    </ul>
-    <div className={style.previewGalleryCover}>
-      {activePhotoIndex + 1} / {photos.length}
+}) => {
+  if (!photos.length) {
+    return null;
+  }
+
+  const previewContainer = useRef<HTMLUListElement>(null);
+
+  useEffect(() => {
+    if (!previewContainer.current) {
+      return;
+    }
+    previewContainer.current.style.transform = `translate3d(-${
+      activePhotoIndex * 164
+    }px, 0, 0)`;
+  }, [activePhotoIndex]);
+
+  return (
+    <div className={cl(style.previewGallery, className)}>
+      {useMemo(
+        () => (
+          <ul className={style.previewGalleryTrack} ref={previewContainer}>
+            {photos.map(photo => (
+              <li key={photo.id} className={style.previewGalleryPreview}>
+                <img
+                  src={photo.preview}
+                  alt={photo.alt}
+                  className={style.previewGalleryImage}
+                />
+              </li>
+            ))}
+          </ul>
+        ),
+        [],
+      )}
+      <div className={style.previewGalleryCover}>
+        {activePhotoIndex + 1} / {photos.length}
+      </div>
     </div>
-  </div>
-);
+  );
+};
